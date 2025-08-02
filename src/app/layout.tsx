85d95fbf3dcd669254e11e15f38fc8f3ed1eb7a9
newbie-del <mikehanderson108@proton.me>
2025-08-02 15:18:29 +0530

    08: trpc setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
-import { Inter, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+
+import { TRPCReactProvider } from "@/trpc/client";
 import "./globals.css";
 
 const inter = Inter({
@@ -17,12 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <TRPCReactProvider>
+      <html lang="en">
       <body
         className={`${inter.className}  antialiased`}
       >
         {children}
       </body>
-    </html>
+       </html>
+    </TRPCReactProvider>
   );
 }
